7ecf49dd4c1a9dbcbad3d1a0101ffb5bb1d85b89
IagoOliveira23 <iago.ishift@gmail.com>
2023-07-27 15:12:49 -0300

    Initial commit

---
+++ src/br/com/iago/cm/excecao/ExplosaoException.java
@@ -0,0 +1,8 @@
+package br.com.iago.cm.excecao;
+
+public class ExplosaoException extends RuntimeException{
+
+    private static final long SerialVersionUID = 1L;
+
+}
+
---
+++ src/br/com/iago/cm/excecao/SairExcrption.java
@@ -0,0 +1,5 @@
+package br.com.iago.cm.excecao;
+
+public class SairExcrption extends RuntimeException{
+    private static  final  long serialVersionUID = 1L;
+}
